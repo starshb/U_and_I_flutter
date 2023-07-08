39641569f0aa9fbf0311c22139cc14f3c12672a5
starshb <gksquf7367@naver.com>
2023-07-08 13:14:10 +0900

    first commit

---
+++ test/widget_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:u_and_i/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+
+    await tester.pumpWidget(const MaterialApp());
+
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
